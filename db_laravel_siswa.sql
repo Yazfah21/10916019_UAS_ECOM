6c9f14184ff3467050a22555bd1c9dcdb5dc656e
Yazfah21 <47276305+Yazfah21@users.noreply.github.com>
2019-02-03 08:19:22 +0700

    Database
    Database

---
+++ db_laravel_siswa.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Feb 2019 pada 15.59
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 7.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_laravel_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2018_12_22_100834_create_table_kelas', 1),
+(2, '2018_12_22_103151_create_table_kelas', 2),
+(3, '2019_01_31_163445_create_table_siswa', 3),
+(4, '2019_01_31_180814_create_table_siswa', 4),
+(5, '2019_01_31_185719_create_table_user', 5),
+(6, '2019_01_31_192447_add_foto_table_siswa', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `t_kelas`
+--
+
+CREATE TABLE `t_kelas` (
+  `id_kelas` int(10) UNSIGNED NOT NULL,
+  `nama_kelas` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jurusan` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `t_kelas`
+--
+
+INSERT INTO `t_kelas` (`id_kelas`, `nama_kelas`, `jurusan`, `created_at`, `updated_at`) VALUES
+(5, 'XII RPL1', 'Rekayasa Perangkat Lunak', '2018-12-22 06:27:43', '2019-02-02 06:43:31'),
+(9, 'XII-RPL5', 'Rekayasa Perangkat Lunak', '2019-02-02 06:44:05', '2019-02-02 06:44:05');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `t_login`
+--
+
+CREATE TABLE `t_login` (
+  `id_login` int(10) UNSIGNED NOT NULL,
+  `nama_user` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `t_login`
+--
+
+INSERT INTO `t_login` (`id_login`, `nama_user`, `username`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin', '$2y$10$j3gD9QmklWB.w0s0.xoqyeuOW79nZIrbZLZyxJVULbkCORzTgYe6W', 'ymLjQPbMPFmN5rRPBiJItjVeBTjaK3PJWqWso11wCxY5uLn4aEftKYxqXKY0', '2019-01-31 12:05:51', '2019-01-31 12:05:51');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `t_siswa`
+--
+
+CREATE TABLE `t_siswa` (
+  `nis` int(11) NOT NULL,
+  `nama_lengkap` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_kelamin` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_telp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `foto` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `t_siswa`
+--
+
+INSERT INTO `t_siswa` (`nis`, `nama_lengkap`, `jenis_kelamin`, `alamat`, `no_telp`, `id_kelas`, `created_at`, `updated_at`, `foto`) VALUES
+(10916019, 'Muhamad Yazril Rizky Fahlevi', 'L', 'Griya Cempaka Arum Blok H1 no 46', '085797512543', 5, '2019-02-02 06:50:42', '2019-02-02 06:50:42', '10916019.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `t_kelas`
+--
+ALTER TABLE `t_kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indeks untuk tabel `t_login`
+--
+ALTER TABLE `t_login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indeks untuk tabel `t_siswa`
+--
+ALTER TABLE `t_siswa`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `t_kelas`
+--
+ALTER TABLE `t_kelas`
+  MODIFY `id_kelas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `t_login`
+--
+ALTER TABLE `t_login`
+  MODIFY `id_login` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
